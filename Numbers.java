c8abb2ab1c4d420edf3ddea95d7648a518e11fce
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-10-01 13:12:47 +0100

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,18 @@
+public class Numbers {
+    public static void main(String args[]) {
+        short s;
+        int i;
+        long l;
+        float f;
+        double d;
+
+        s = 10;
+        i = 1000;
+        l = 1000000L;
+        f = 230.47f;
+        d = 30949.374;
+
+        System.out.println("s: "+s);
+        System.out.format("\ni: %d\n", i);
+    }
+}
